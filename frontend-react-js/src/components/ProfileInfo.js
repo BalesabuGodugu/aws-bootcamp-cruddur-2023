ac6214bc0ebb0e5843a3dd1cea227459b442ea4c
Balesabu Godugu <godugubalesabu@gmail.com>
2023-03-04 18:23:37 +0000

    updated ui for congnito configuration

--- frontend-react-js/src/components/ProfileInfo.js
+++ frontend-react-js/src/components/ProfileInfo.js
@@ -3,7 +3,16 @@ import {ReactComponent as ElipsesIcon} from './svg/elipses.svg';
 import React from "react";
 
 // [TODO] Authenication
-import Cookies from 'js-cookie'
+import { Auth } from 'aws-amplify';
+
+const signOut = async () => {
+  try {
+      await Auth.signOut({ global: true });
+      window.location.href = "/"
+  } catch (error) {
+      console.log('error signing out: ', error);
+  }
+}
 
 export default function ProfileInfo(props) {
   const [popped, setPopped] = React.useState(false);
